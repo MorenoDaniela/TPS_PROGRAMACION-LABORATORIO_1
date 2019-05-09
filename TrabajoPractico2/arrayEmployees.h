3e34d61a3d37c600b992c968d03202e6d48c8ee0
MorenoDaniela <47337445+MorenoDaniela@users.noreply.github.com>
2019-05-09 16:51:05 -0300

    faltan algunos cambios.

---
+++ TrabajoPractico2/arrayEmployees.h
@@ -0,0 +1,24 @@
+#ifndef ARRAYEMPLOYEES_H_INCLUDED
+#define ARRAYEMPLOYEES_H_INCLUDED
+
+typedef struct
+{
+    int id;
+    char name[51];
+    char lastName[51];
+    float salary;
+    int sector;
+    int isEmpty;
+}Employee;
+
+int initEmployees(Employee* lista, int len);
+int addEmployees(Employee* lista,int id,char name[51],char lastName[51],float salary,int sector,int len);
+int findEmptyPlace (Employee* lista,int len, int* result);
+void printArray(Employee* lista, int len);
+int findEmployeeById(Employee* lista,int id, int len);
+int removeEmployee(Employee* lista, int id, int len);
+int modifyEmployees(Employee* lista, int id, int len);
+int sortEmployees(Employee* lista, int order, int len);
+int averageSalary(Employee* lista, int len);
+
+#endif // ARRAYEMPLOYEES_H_INCLUDED
